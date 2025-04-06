ee5f835681d19bc8b20aa69673d73f64ce6be6ab
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-06 20:24:19 +0330

    2

--- faz1.py
+++ faz1.py
@@ -1,6 +1,14 @@
-class Preson:
-    pass
+class Movie:
+
+    def __init__(self, title, director, year):
+        self.title = title
+        self.dierctor = director
+        self.year = year
+
+    def info(self):
+        print(f"title= {self.title}  director= {self.dierctor} year= {self.year}")
 
 
-print("test")
-print("test2")
+m1 = Movie("gol", "reza", 1988)
+inf = m1.info()
+print(inf)

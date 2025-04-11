5d9247350926cb8a1d3db0bc3f3a5584e749f406
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-11 21:22:38 +0330

    16

--- 20250411/__init__.py
+++ 20250411/__init__.py
@@ -1,19 +1,27 @@
 # __init__
 class Student:
-    def __init__(self, name, grade):
+    def __init__(self, name, grade, gen):
         self.name = name
         self.grade = grade
+        self.gen = gen
 
     def is_passing(self):
         if self.grade >= 60:
-            return True
+            return "passed"
         else:
-            return False
+            return "unpassed"
+
+    def gender(self):
+        if self.gen == "f":
+            return f"{self.name} is Female"
+        elif self.gen == "m":
+            return f"{self.name} is Male"
 
 
-s1 = Student("reza", 60)
-s1.is_passing()
-s1.name
-s1.grade
+s1 = Student("reza", 60, "m")
+print(s1.is_passing())
+print(s1.name)
+print("*" * 40)
+s1.gender()
 print(f"{Student.__name__}")
 print(f"{s1.name}  {s1.grade}")

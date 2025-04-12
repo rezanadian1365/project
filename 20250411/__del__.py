5ae6aad17fd98df4ac12875bb32f4a0a21542ed7
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-12 20:23:56 +0330

    17

--- 20250411/__del__.py
+++ 20250411/__del__.py
@@ -1,4 +1,17 @@
 # __del__
-class FileSimilatur:
-    def __init__(self, name):
-        self.name = name
+class FileManager:
+    def __init__(self, jik):
+        self.jik = open(jik, "w")
+
+    def writer(self, shalgham):
+
+        self.jik.write(shalgham)
+
+    def __del__(self):
+        self.jik.close()
+        print(f"{self.jik} this time is deleting . . . ")
+
+
+f = FileManager("new.txt")
+f.writer("meat berger")
+del f

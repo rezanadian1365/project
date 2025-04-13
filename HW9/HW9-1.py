5bcbbb5c5d9c9be0cbf93c7a5cc833c214179c75
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-13 20:12:18 +0330

    23

--- HW9/HW9-1.py
+++ HW9/HW9-1.py
@@ -1,5 +1,26 @@
 # singleton
-#enheritance
-#classmethod
-#instancemethod
+# enheritance
+# classmethod
+# instancemethod
 class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance == None:
+            cls._instance = super().__new__(cls)
+            print("class method is runnung")
+        return cls._instance
+
+    def __init__(self):
+        print("\/instance method is running\/")
+
+
+akbar = Singleton()
+print("*" * 30)
+asghar = Singleton()
+print("*" * 30)
+javad = Singleton()
+answer = akbar is asghar
+print("*" * 30)
+print(f"           {answer}")
+id(akbar) == id(asghar)

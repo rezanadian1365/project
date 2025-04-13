101a946bf9a029abc3883255d6478d0784e42106
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-13 21:24:41 +0330

    24

--- HW9/add.py
+++ HW9/add.py
@@ -1,4 +1,16 @@
 # class Add:
 class Add:
-    def __init__(self):
-        pass
+    def __init__(self, *args: int):
+        for arg in args:
+            if not isinstance(arg, int):
+                raise TypeError("args just intiger")
+        self.args = args
+
+    def add(self):
+
+        res = sum(self.args)
+        print(f"{res}")
+
+
+a = Add(2, 5, 3, 4)
+print(a.add())

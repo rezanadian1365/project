15adfe9abcbfc7203facc3ccceb25d7065e16e30
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-15 22:08:28 +0330

    34

--- HW9/property.py
+++ HW9/property.py
@@ -1,1 +1,21 @@
 # decorator
+class Person:
+    def __init__(self, name, birth_day):
+        self.name = name
+        self.birth_day = birth_day
+
+    # @property
+    def age(self):
+        from datetime import datetime as dt
+
+        return dt.now().year - self.birth_day
+
+
+def main():
+
+    p = Person("Ali", 1987)
+    print(p.age())
+
+
+if __name__ == "__main__":
+    main()

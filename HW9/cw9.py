27906192a6c688b4351247e594a258e121e582d6
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-15 21:09:16 +0330

    32

--- HW9/cw9.py
+++ HW9/cw9.py
@@ -1,33 +1,51 @@
-fron enum import Enum
+from enum import Enum
+
+
 class Actions(Enum):
     pass
-class Character:
-    BUFF=1
-    NORMAL_ATTACK=2
-    SPECIAL_ATTACK=3
-    def __init__(self,hp,power):
 
-        self.hp=hp
-        self.power=power
+
+class Character:
+    BUFF = 1
+    NORMAL_ATTACK = 2
+    SPECIAL_ATTACK = 3
+
+    def __init__(self, hp, power):
+
+        self.hp = hp
+        self.power = power
+
     def recive_damage(self):
         pass
+
+
 class Wariorr(Character):
     def iron_skin(self):
         pass
+
     def blade_storm(self):
         pass
+
+
 class Mage(Character):
     def arcane_shield(self):
         pass
+
     def fireball(self):
         pass
+
+
 class Assissin(Character):
     def assassin_instinct(self):
         pass
+
     def laserat(self):
         pass
+
+
 class Battle:
     def __init__(self):
         pass
+
     def play_turn(self):
         pass

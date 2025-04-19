524ba68d8580ecd38fd044367ec5a815058607e9
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-19 22:23:54 +0330

    54

--- HP10/mixin.py
+++ HP10/mixin.py
@@ -4,3 +4,19 @@
 # The string_to_list method takes a string as input and returns a list of words in the string.
 # the mixin class is used to add functionality to another class without modifyting the orginal class.
 # This ALLOWS for code reuse and better organization.
+class StringTolistMixin:
+    def string_to_list(self, string):
+        """convert a string to a list of words"""
+        return string.split()
+
+
+class Test(StringTolistMixin):
+    def __init__(self, string):
+        self.string = string
+
+    def __str__(self):
+        return self.string_to_list(self.string)
+
+
+t1 = Test("hello world ")
+print(t1.__str__())

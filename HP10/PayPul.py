e5daf63476689cb81bdc2b4c8f2bbbb5ae15d5a4
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-19 22:07:50 +0330

    52

--- HP10/PayPul.py
+++ HP10/PayPul.py
@@ -1,1 +1,13 @@
 # PayPul
+# with use of ABC and abstractmethod
+from abs import ABC, abstractmethod
+
+
+class PayPal(ABC):
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+    @abstractmethod
+    def refund(self, amount):
+        pass

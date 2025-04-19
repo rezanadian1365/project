1d2e6b48282cbddceab2d8c7ad516d38727cf20a
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-19 22:14:34 +0330

    53

---
+++ HP10/mixin.py
@@ -0,0 +1,6 @@
+# mixin
+# This file contains a mixin class that provides a method to convert a string to a list of words.
+# The mixin class is called StringTolistMixin and it has a method called string_to_list.
+# The string_to_list method takes a string as input and returns a list of words in the string.
+# the mixin class is used to add functionality to another class without modifyting the orginal class.
+# This ALLOWS for code reuse and better organization.

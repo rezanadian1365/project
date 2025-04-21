7650465726123582f29bf3ba343803a3ab03949a
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-21 20:37:26 +0330

    60

--- HW10/my_ticket_system/auth.py
+++ HW10/my_ticket_system/auth.py
@@ -1,1 +1,20 @@
-# Auth
+class Auth:
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password
+
+    def login(self):
+        self.username = input("Enter username:")
+        self.password = input("Enter password:")
+
+    def validate_user(self):
+        with open("userpass.txt", "r") as f:
+            for line in f:
+                user, passw = line.strip().split("=")
+                if user == self.username and passw == self.password:
+                    print("login successful")
+                    return True
+
+
+admin = Auth()
+admin.login()
